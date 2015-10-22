fec199e979e04ea67183304bff23c638abbd3990
Herb Miller <herb@bobbingwide.com>
2015-10-22 15:48:52 +0100

    v0.0.2 - milestone v0.0.2
    Fixes #1 - schunter should cater for script and CDATA sections
    Fixes #2 - schunter should cater for style tags
    Fixes #3 - schunter should cater for shortcodes which don't expand $content
    Fixes #4 - schunter should cater for postmeta and options, including serialized data

--- admin/class-schunter.php
+++ admin/class-schunter.php
@@ -4,7 +4,7 @@
  *
  * Class Schunter
  * 
- * Implements the Short Code Hunter 
+ * Implements the Short Code Hunter ( schunter ) 
  * 
  */
 class Schunter {
@@ -44,6 +44,20 @@ class Schunter {
 	public $done = 0;
 	
 	/**
+	 * Recently found shortcodes
+	 * 
+	 * Array of codes we're building up while schunting
+	 *
+	 */															
+	public $found_codes = array();
+	
+	/**
+	 * Skip to here.
+	 * 
+	 */
+	public $skipping_to = null;
+	
+	/**
 	 * Contruct the Schunter
 	 */
 	function __construct() {
@@ -62,19 +76,49 @@ class Schunter {
 	 *
 	 * There are multiple stages to building the information that maps shortcode usage 
 	 * First and foremost is understanding the usage of shortcodes in posts and postmeta
+	 * then we look at options - particularly widget_text
+	 * and then all the other places, including multisite tables
+	 
+	 	field                         table            | Notes
+		--------------------------- | ---------------- | ---------------------------------------
+	  public $commentmeta_refs;	  | wp_commentmeta   | probably not a good idea to expand shortcodes in comment meta data
+	  public $comments_refs;	    | wp_comments			 | probably not a good idea to expand shortcodes in comments
+	  public $links_refs;				  | wp_links         | Deprecated since WordPress 3.5
+	y public $options_refs;			  | wp_options       | Not stored in the option_name but can be in any option value. May be in serialized data
+	y public $postmeta_refs;		  | wp_postmeta      | Not stored in the meta_key but can be in any meta_value. May be in serialized data
+	y public $posts_refs;				  | wp_posts         | Check post_title, post_content, post_excerpt 
+	  public $sitemeta_refs; 		  | wp_sitemeta      | Not stored in the meta_key but can be in any meta_value. May be in serialized data
+	  public $term_meta_refs;		  | wp_term_meta     | Not expected prior to WordPress 4.4?
+	  public $term_taxonomy_refs; | wp_term_taxonomy | Check description
+	  public $terms_refs;				  | wp_terms         | In the name? Perhaps not! 
+	  public $usermeta_refs;		  | wp_user_meta     | Not stored in meta_key but can be in any meta_value. May be in serialized data
+	  public $users_refs;				  | wp_users         | In the display_name? Perhaps not!
+	  public $widget_refs;			  | wp_options       | Definitely in serialized data: option_name: 'widget_text'
+	 
 	 */
 	function run() {
-		echo "Schunting for shortcodes";
+		echo "Schunting for shortcodes" . PHP_EOL;
 		$this->codes->fetch();
 		$this->options->fetch();
 		$this->schunt_posts();
+		
+		//$this->schunt_comments();
+		//$this->schunt_links();
+		$this->schunt_options();
+		//$this->schunt_terms();
+		//$this->schunt_users();
+		
 		
 		// $this->schunt_
 		$this->end_process();
 	}
 	
 	/**
-	 * 	Search posts and postmeta for possible shortcodes
+	 * Search posts and postmeta for possible shortcodes
+	 *
+	 * Note: We don't see posts which are not publicly searchable, so we don't see "revisions".  
+	 * This is not considered to be a problem right now. 
+	 * To fix this we can temporarily hack with the registered post types so that the query finds more things
 	 *
 	 */
 	function schunt_posts() {
@@ -128,6 +172,7 @@ class Schunter {
 	}
 	
 	/**
+	 * Mark another post as completed
 	 * 
 	 */
 	function done( $post ) {
@@ -149,8 +194,6 @@ class Schunter {
 	/**
 	 * Hunt for shortcodes in the post
 	 *
-	 * @TODO - Do we see revisions here? If no, does it matter?
-	 *
 	 */		
 	function schunt_post( $post ) {
 		echo "{$post->ID} {$post->post_type} {$post->post_modified}" . PHP_EOL;
@@ -158,7 +201,7 @@ class Schunter {
 		$codes = $this->schunt_codes( $post->post_title );
 		$codes += $this->schunt_codes( $post->post_content );
     $codes += $this->schunt_codes( $post->post_excerpt );
-		$this->codes->add_codes( $codes, $post->ID, "posts" );
+		$this->add_codes( $post->ID, "posts" );
 	}	
 	
 	/**
@@ -174,9 +217,61 @@ class Schunter {
 				$codes += $this->schunt_codes( $value );
 			}
 		}
-		$this->codes->add_codes( $codes, $post->ID, "postmeta" );
+		$this->add_codes( $post->ID, "postmeta" );
 	}
 	
+	/**
+	 * Hunt for shortcodes in options
+	 *
+	 * Note: This includes transients and widget text.
+	 *
+	 * @TODO Filter out options we're not interested in. e.g. 
+	 * - exploitscanner_results
+	 * - rewrite_rules
+	 * - _site_transient_update_plugins
+	 *
+	 */
+	function schunt_options() {
+		$options = $this->load_alloptions();
+		if ( count( $options ) ) {
+			//$codes = array();
+			foreach ( $options as $option_key => $option_value ) {
+				echo "Schunting option: $option_key" . PHP_EOL;
+				if ( $option_key == "exploitscanner_results" ) {
+					continue;
+				}
+				if ( $option_key == "rewrite_rules" ) {
+					continue;
+				}
+				$codes = $this->schunt_codes( $option_value );    
+				$this->add_codes( $option_key, "options" );
+			}
+		}
+	}
+
+	/**
+	 * Get all options containing '[', including those with autoload='no'
+	 *
+	 * @TODO There's some belt and braces stuff going on here. Remove unnecessary test when satisfied with results.
+	 * 
+	 */
+	function load_alloptions() {
+		global $wpdb;
+
+    //$suppress = $wpdb->suppress_errors();
+    $alloptions_db = $wpdb->get_results( "SELECT option_name, option_value FROM $wpdb->options where option_value like '%[%'" );
+    //$wpdb->suppress_errors($suppress);
+		//print_r( $alloptions_db );
+    $alloptions = array();
+    foreach ( (array) $alloptions_db as $o ) {
+			if ( false !== strpos( $o->option_value, "[" ) ) {
+				$alloptions[$o->option_name] = $o->option_value;
+				echo $o->option_name . PHP_EOL;
+			}
+    }
+		return $alloptions;
+	}
+
 	/**
 	 * Hunt for anything that looks like a shortcode
 	 *
@@ -184,49 +279,207 @@ class Schunter {
 	 * All we need to find is shortcodes
 	 * So we can simply break the content down by exploding on the '['s
 	 * Here we find both start and ending shortcodes...
-	 * There is a possibility that the ending shortcode is not the same as the starting one
+	 *
+	 * If the content has been constructed incorrectly, then there is a possibility that the ending shortcode is not the same as the starting one.
+	 * But we don't really care about this. 
+	 *
+	 *
+	 * We need to be able to detect serialized data and probably JSON or CSS and process it accordingly.
+	 * use maybe_unserialize() for the serialized data?
+	 * 
 	 * 
 	 * @param string $text which may contain a shortcode
 	 * @return array $codes the things we think are shortcodes
 	 *
 	 */
 	function schunt_codes( $text ) {
-		$codes = array();
+		$schunt = $this->schunt( $text );
+		if ( $schunt ) {
+			echo "Length: " . strlen( $text ) . PHP_EOL;
+			//docontinue();
+			$deser = maybe_unserialize( $text ); 
+			if ( is_array( $deser ) ) {
+				echo "Deser: " . count( $deser ) . PHP_EOL;
+				$codes = $this->schunt_codes_from_array( $deser );
+			} else {
+				$codes = $this->schunt_codes_from_text( $text ); 
+			}
+		} else { 
+			$codes = array();
+		}
+		return( $codes );
+	}
+	
+	function schunt( $text ) {
 		if ( false !== strpos( $text, "[" ) ) {
-			$bits = explode( "[", $text ); 
-			//print_r( $bits );
-			array_shift( $bits );
-			foreach ( $bits as $bit ) {
-				if ( strlen( $bit ) > 0 ) {
-					$bit = strtr( $bit, "/]", "  " );
-					$bit = rtrim( $bit );
-					$sc = explode( " ", $bit ); 
-					$code = $sc[0];
-					//echo "Code: $code" . PHP_EOL;
-					$invalid = preg_match( '@[<>&/\[\]\x00-\x20=]@', $code );
-			 
-					if ( $invalid ) {
-						echo "Invalid shortcode detected? $code" . PHP_EOL;
-						echo $bit . PHP_EOL;
-						echo PHP_EOL;
-						//echo $text . PHP_EOL;
-						
-						//gobang();
-						//do_continue();
-						//oikb_get_response();
-				 } else {
-            //echo "Code: $code looks like a good un" . PHP_EOL;
-						$codes[ $code ] = $code;
-					} 
-				} 
+			$schunt = true;
+		} else {
+			$schunt = false;
+		}
+		return( $schunt );
+	}
+	
+	/**
+	 * Re-initialise found_codes
+	 */
+	function schunt_start() {	
+		$this->found_codes = array();
+	}
+	
+	/**
+	 * Add the found codes
+	 *
+	 * Update the $codes with the found codes
+	 * then reset for next time
+	 *
+	 * @param string|integer $id Identifier for the content
+	 * @param string $class Content type
+	 */
+	function add_codes( $id, $class ) {
+		$this->codes->add_codes( $this->found_codes, $id, $class );
+		$this->schunt_start();
+	}
+		
+	/**
+	 * Extract shortcodes from a text string
+	 *
+	 * First we break the string down into chunks of HTML
+	 * then we process the shortcodes within the HTML
+	 * or the shortcodes in plain text
+	 * Ignoring sections where we don't expect to see shortcodes
+	 * 
+	 *
+	 * @param string $text a text field
+	 * @return array of shortcodes, may be an empty array 
+	 */
+	function schunt_codes_from_text( $text ) {
+		$codes = array();
+		if ( $this->schunt( $text ) ) {
+			$text_array = wp_html_split( $text );
+			//print_r( $text_array );
+			foreach ( $text_array as $text ) {
+				if ( $this->skip( $text ) ) {
+					continue;
+				}	
+			
+				if ( $this->schunt( $text ) ) {
+					$bits = explode( "[", $text ); 
+					//print_r( $bits );
+					array_shift( $bits );
+					
+					foreach ( $bits as $bit ) {
+						if ( $this->skip( $bit ) ) {
+							continue;
+						}
+						if ( strlen( $bit ) > 0 ) {
+							$bit = strtr( $bit, "/]", "  " );
+							$bit = rtrim( $bit );
+							$sc = explode( " ", $bit ); 
+							$code = $sc[0];
+							//echo "Code: $code" . PHP_EOL;
+							$invalid = preg_match( '@[<>&/\[\]\x00-\x20=]@', $code );
+					
+							if ( $invalid ) {
+								echo "Invalid shortcode detected? $code" . PHP_EOL;
+								echo $bit . PHP_EOL;
+								echo PHP_EOL;
+								echo $text . PHP_EOL;
+								
+								//gobang();
+								docontinue();
+								//oikb_get_response();
+						 } else {
+							 //echo "Code: $code looks like a good un" . PHP_EOL;
+								$codes[ $code ] = $code;
+								$this->found_codes[ $code ] = $code;
+								$this->check_code_skip( $code );
+							} 
+						} 
+					}	
+				}
 			}
 		}
 		//print_r( $codes );
 		//gobang();
 		return( $codes );			
-			
 	}
 	
+	/**
+	 * Determine if were skipping
+	 *
+	 * @TODO Decide if we should also ignore the tags returned from 'no_texturize_tags'
+	 * $default_no_texturize_tags = array('pre', 'code', 'kbd', 'style', 'script', 'tt');
+	 *
+	 * @TODO Should these tests be case insensitive?
+	 *
+	 * @param string $text line to test
+	 * @return string the current value of skipping_to
+	 */
+	function skip( $text ) {
+		//print_r( $text );
+		//docontinue( "test_skip: {$this->skipping_to}" );
+    if ( $this->skipping_to ) {
+			if ( 0 === strpos( $text, $this->skipping_to ) ) {
+				$this->skipping_to = null;	
+			}
+		} else {
+			if ( 0 === strpos( $text, "<script" ) ) {
+				$this->skipping_to = "</script";
+			}elseif ( 0 === strpos( $text, "<style" ) ) {
+				$this->skipping_to = "</style";
+			} 
+		}	
+		return( $this->skipping_to );
+	} 
+	
+	/**
+	 * Check if we should be skipping shortcodes in content
+	 *
+	 * @TODO This is just a prototype using a known shortcode where we don't expect shortcodes since the content is CSS.
+	 * We could use the 'no_texturize_shortcodes' filter
+	 *
+	 * Other examples:
+	 * - code - is in $default_no_texturize_shortcodes
+	 * - bw_geshi
+	 * - bw_graphviz 
+	 * 
+	 * 
+	 * @param string $code - the code we're testing for
+	 */
+	function check_code_skip( $code ) {
+		static $no_texturize_shortcodes = null;
+		if ( !$no_texturize_shortcodes ) {
+			$no_texturize_shortcodes = apply_filters( "no_texturize_shortcodes", array( "codes" ) );
+			$no_texturize_shortcodes = bw_assoc( $no_texturize_shortcodes );
+		} 
+		if ( bw_array_get( $no_texturize_shortcodes, $code, null ) ) {
+			$this->skipping_to = "/$code";
+		}
+	}
+	
+	/**
+	 * Extract shortcodes from an array 
+	 *
+	 * We assume the array is deserialized data
+	 * We need to process each child array, so we do it recursively
+	 *
+	 * @param array $deser array of fields which may themselves be array
+	 */
+	function schunt_codes_from_array( $deser ) {
+		foreach ( $deser as $key => $value ) {
+			if ( is_array( $value ) ) {
+				$this->schunt_codes_from_array( $value );
+			} else {
+				if ( $this->schunt( $value ) ) {
+					$this->schunt_codes_from_text( $value );
+				}	
+			}		
+		}
+	}
+	
+	/**
+	 * Produce a simple report of the shortcodes
+	 */
 	function report() {
 		print_r( $this->options );
 		//print_r( $this->codes );
