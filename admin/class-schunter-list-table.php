d57b7df847802b110db2f7ead5463145ec3530ee
Herb Miller <herb@bobbingwide.com>
2015-11-03 10:15:42 +0000

    Start putting content into the table
    - Generalise the sorting logic
    - Sort needs to use populate_orderby_field() to populate the complete list of items before select_items()
    - Move Function before References
    - Add Status to the sortable columns
    - Invoke populate_fields() after load_items() in prepare_items()
    
    Also, don't use esc_url() in reset_request_uri()

--- admin/class-schunter-list-table.php
+++ admin/class-schunter-list-table.php
@@ -17,6 +17,22 @@ class Schunter_List_Table extends BW_List_Table {
 	 *
 	 */
 	public $codes;
+	
+	
+	public $total_items;
+	
+	
+	/** 
+	 * Sort order column
+	 * 
+	 * code or total_references 
+	 */
+  public $orderby = null;	 	
+	
+	/** 
+	 * Sort order - ASC or DESC
+	 */
+	public $order = null;
 
 	/**
 	 * @var Schunter_List_Table - the true instance
@@ -35,7 +51,7 @@ class Schunter_List_Table extends BW_List_Table {
 		return self::$instance;
 	}
 	
-	public $total_items;
+	
 
 
 	/**
@@ -53,8 +69,23 @@ class Schunter_List_Table extends BW_List_Table {
 		$columns['cb'] = '<input type="checkbox" />';
 		$columns['code'] = __( "Code", "schunter" ); 
 		$columns['status'] = __( "Status", "schunter" );
+		$columns['function'] = __( 'Function', 'schunter' );
 		$columns['total_references'] = __( 'References', 'schunter' );
+		bw_backtrace();
+		bw_trace2( $columns, "columns", false );
 		return( $columns );	
+	}
+	
+	/**
+	 * Get sortable columns 
+	 */
+	public function get_sortable_columns() {
+		$sortable_columns = array();
+		$sortable_columns['code'] = array( "code", false );
+		$sortable_columns['status'] = array( "status", false );
+		$sortable_columns['total_references'] = array( "total_references", false );
+		bw_trace2();
+		return( $sortable_columns );
 	}
 
 	/**
@@ -66,13 +97,14 @@ class Schunter_List_Table extends BW_List_Table {
 	 * 
 	 */
 	public function prepare_items() {
-		$columns = get_column_headers( $this->screen );
-		$hidden = array();
-		$sortable = array(); 
-		$this->_column_headers = array( $columns, $hidden, $sortable );  
+		//$columns = get_column_headers( $this->screen );
+		//$hidden = array();
+		//$sortable = $this->get_sortable_columns();
+		//$this->_column_headers = array( $columns, $hidden, $sortable );  
 		//
 		$this->reset_request_uri();
 		$this->load_items(); 
+		$this->populate_fields();            
 		bw_trace2( $this, "prepared_items", false, BW_TRACE_DEBUG );
 		}
 
@@ -90,23 +122,30 @@ class Schunter_List_Table extends BW_List_Table {
 		$atts = array();
 		$atts = $this->determine_pagination( $atts ); 
 		$this->sort_items();
-		$this->select_items( $atts );            
+		$this->select_items( $atts );
 		$this->record_pagination( $atts );
 	}
 
-/**
+	/**
 	 * Ensure the pagination links don't attempt to perform any actions
 	 * 
 	 * REQUEST_URI is used by BW_List_Table::pagination() to build the paging links
 	 * we need to ensure that only pagination is performed.
 	 * So we need to remove the fields that can be set on the action links
 	 * ie. action should not be set.
-	 *  
+	 * 
+	 *
+	 * So when do we remove_query_arg() for orderby and order?
+	 *
+	 * Note: Don't call esc_url() when setting REQUEST_URI since this prevents
+	 * add_query_arg() and remove_query_arg() from working properly.
 	 */
 	function reset_request_uri() {
 		//$request_uri = $_SERVER['REQUEST_URI'];
 		$request_uri = remove_query_arg( array( "action" ) );
-		$_SERVER['REQUEST_URI'] = esc_url( $request_uri );
+		//bw_trace2( $request_uri, 'REQUEST_URI', false );
+		$_SERVER['REQUEST_URI'] = $request_uri;	 // Don't do esc_url() here!
+		//bw_trace2( $_SERVER['REQUEST_URI'], 'server REQUEST_URI', false );
 	} 
 
 	
@@ -145,7 +184,99 @@ class Schunter_List_Table extends BW_List_Table {
                             [table_refs] => 
                             [php_refs] => 
                         )
-	*/ 
+	
+	
+	$data is a Schunter_code object
+	
+	
+	    [code] => 
+    [function] => 
+    [status] => 
+    [total_references] => 52
+    [comments_refs] => 
+    [commentmeta_refs] => 
+    [links_refs] => 
+    [options_refs] => Array
+        (
+            [widget_text] => widget_text
+        )
+
+    [postmeta_refs] => 
+    [posts_refs] => Array
+        (
+            [31740] => 31740
+            [28299] => 28299
+            [258] => 258
+            [307] => 307
+            [315] => 315
+            [327] => 327
+            [339] => 339
+            [860] => 860
+            [28727] => 28727
+            [362] => 362
+            [18262] => 18262
+            [387] => 387
+            [397] => 397
+            [423] => 423
+            [453] => 453
+            [29119] => 29119
+            [29128] => 29128
+            [468] => 468
+            [471] => 471
+            [483] => 483
+            [506] => 506
+            [528] => 528
+            [532] => 532
+            [593] => 593
+            [29251] => 29251
+            [653] => 653
+            [656] => 656
+            [673] => 673
+            [772] => 772
+            [1014] => 1014
+            [663] => 663
+            [4698] => 4698
+            [29343] => 29343
+            [29410] => 29410
+            [29424] => 29424
+            [8351] => 8351
+            [29585] => 29585
+            [29744] => 29744
+            [29771] => 29771
+            [874] => 874
+            [911] => 911
+            [30018] => 30018
+            [29916] => 29916
+            [30057] => 30057
+            [28051] => 28051
+            [30089] => 30089
+            [804] => 804
+            [30118] => 30118
+            [31678] => 31678
+            [31653] => 31653
+            [31679] => 31679
+        )
+
+    [sitemeta_refs] => 
+    [term_meta_refs] => 
+    [term_taxonomy_refs] => 
+    [terms_refs] => 
+    [usermeta_refs] => 
+    [users_refs] => 
+    [widget_refs] => 
+    [table_refs] => 
+    [php_refs] => 
+)
+  */
+	
+	function populate_fields() {
+		foreach ( $this->items as $item => $code ) {
+			bw_trace2( $item, "item", false );
+			bw_trace2( $code, "code", false );
+			$code->status();
+			
+		}
+	} 
 		/**
 	 * Determine pagination parameters
 	 * 
@@ -198,14 +329,53 @@ class Schunter_List_Table extends BW_List_Table {
 	
 	}
 	
+	/**
+	 * Sort the full list of items
+	 * 
+	 * When the items are not loaded by WP_Query() then we need to put them in the required order manually
+	 * using the defined sort sequence. 
+	 *
+	 * Note: WP_List_Table doesn't cater for sorting on multiple columns, so we don't either
+	 */
 	function sort_items() {
+		$this->orderby();
+		$this->order();
+		$this->populate_orderby_field(); 
 		usort( $this->items, array( $this, "sort_objects_by_code" ) );
 	}
 	
 	/**
-	 * Sort code objects by 'code'
-	 * 
-	 * This sorts ASC. If we want DESC we can simply negate the $result
+	 * Set the orderby value for sorting
+	 *
+	 * @TODO - Determine the default sort sequence from the sortable part of get_sortable_columns()
+	 * AND or validate the sort column from this information
+	 *
+	 */
+	function orderby() {
+		$this->orderby = bw_array_get( $_GET, "orderby", "code" );
+	}
+	
+	/**
+	 * Set the sort sequence - 'asc' or 'desc'
+	 *
+	 * @TODO Validate the order as 'asc' or 'desc'
+	 *
+	 */
+	function order() {
+		$this->order = bw_array_get( $_GET, "order", "asc" );
+	}
+	
+	function populate_orderby_field() {
+		$orderby = $this->orderby;
+		foreach ( $this->items as $item => $code ) {
+			if ( !$code->{$orderby} ) {
+				$code->{$orderby}();
+			}
+		}
+	}
+	
+	/**
+	 * Sort objects
 	 *
 	 * @TODO Should we concern ourselves about case sensitivity?
 	 * 
@@ -237,15 +407,21 @@ class Schunter_List_Table extends BW_List_Table {
             [table_refs] => 
             [php_refs] => 
 			`
+	 * @param object $a - first item to be sorted
+	 * @param object $b - second item to be sorted
+	 * @return integer -1 if a to be before b, 0 if equal, 1 if a to be after b
 	 */
-	
 	function sort_objects_by_code( $a, $b ) {
-		if ( $a->code == $b->code )  { 
+		$property_name = $this->orderby;
+		if ( $a->{$property_name} == $b->{$property_name} )  { 
 			$result = 0; 
-		} elseif ( $a->code < $b->code ) {
+		} elseif ( $a->{$property_name} < $b->{$property_name} ) {
 			$result = -1;
 		} else {
 			$result = 1;
+		}
+		if ( $this->order == "desc" ) {
+			$result = -$result;
 		}
 		return( $result );
 	}
